ffef3dd616b66196dc43a440cb562746d427107e
Huy Nguyen <qhuy1911@gmail.com>
2024-06-16 23:28:46 +0700

    Handle create and delete link

--- backend/src/controllers/linkController.js
+++ backend/src/controllers/linkController.js
@@ -1,19 +1,5 @@
 import { LinkModel } from "../models/index.js"
 
-const getAllLinks = (req, res) => {
-  LinkModel
-    .find()
-    .then((data) => {
-      res.status(200).send({
-        message: "Get all Links successfully",
-        data: data,
-      })
-    })
-    .catch((err) => {
-      res.status(500).send({message: err});
-    })
-}
-
 const getLinksByLoggedInUser = (req, res) => {
   const { userId } = req;
   LinkModel
@@ -30,7 +16,48 @@ const getLinksByLoggedInUser = (req, res) => {
     })
 }
 
+const createLink = (req, res) => {
+  const { userId } = req;
+  const { link, product_name} = req.body;
+  const newLink = new LinkModel({
+    link,
+    product_name,
+    user_id: userId,
+    is_deleted: false,
+  })
+  newLink
+    .save()
+    .then(() => {
+      res.status(201).send({
+        message: "Link was created successfully!"
+      })
+    })
+    .catch(err => {
+      res.status(500).send({message: error.message});
+    })
+}
+
+const deleteLink = (req, res) => {
+  const { linkId } = req.params;
+  LinkModel.findByIdAndUpdate(linkId, {
+    is_deleted: true,
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Liknk with id=${linkId}.`
+        });
+      } else res.send({ message: "Link was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: `Error updating Link with id=${linkId}`
+      });
+    });
+}
+
 export default {
-  getAllLinks,
   getLinksByLoggedInUser,
+  createLink,
+  deleteLink,
 };
--- backend/src/routes/linkRouter.js
+++ backend/src/routes/linkRouter.js
@@ -4,7 +4,8 @@ import { LinkController } from "../controllers/index.js";
 
 const linkRouter = express.Router();
 
-linkRouter.get("", [authJwt.verifyToken], LinkController.getAllLinks)
 linkRouter.get("/me", [authJwt.verifyToken], LinkController.getLinksByLoggedInUser);
+linkRouter.post("", [authJwt.verifyToken], LinkController.createLink);
+linkRouter.put("/:linkId", [authJwt.verifyToken], LinkController.deleteLink);
 
 export default linkRouter;
